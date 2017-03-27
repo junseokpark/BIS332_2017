38bbb8aedcd81e906b82da588e9aa925ccf93b2e
Junseok Park <junseokpark@biosoft.kaist.ac.kr>
2017-03-27 14:17:36 +0900

    week5 code

---
+++ src/main/java/edu/kaist/biosoft/bis332/hello.java
@@ -0,0 +1,10 @@
+package edu.kaist.biosoft.bis332;
+
+/**
+ * Created by Junseok Park on 2017-03-27.
+ */
+public class hello {
+    public static void main (String [] args) {
+        System.out.println("Hello World\n");
+    }
+}
